fe16305f81d3998c728210c9a695790e0b2a928b
Aleksey <llkreator@outlook.com>
2017-11-30 21:13:20 +0200

    g

--- docs/statics/comments.js
+++ docs/statics/comments.js
@@ -1,62 +1,62 @@
 export default [
   {
     comment: 'comment1',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'comment1',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentaie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'comment1',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../img/derkash.jpg',
     name: 'Vlad'
   },
   {
