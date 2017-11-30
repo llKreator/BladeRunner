4299414e62cc8f0f80796a398ec96c3269cc04b9
Aleksey <llkreator@outlook.com>
2017-11-30 21:26:09 +0200

    f

--- docs/statics/comments.js
+++ docs/statics/comments.js
@@ -1,90 +1,90 @@
 export default [
   {
     comment: 'comment1',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'comment1',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentaie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommetarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'comment1',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: '../img/derkash.jpg',
+    avatar: ../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'comment1',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment: 'test comment 2',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../assets/derkash.jpg',
     name: 'Vlad'
   },
   {
     comment:
         'Commentarie Ryan Commentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyanCommentarie Ryan koCommentarie RyankoCommentarie Ryan',
-    avatar: 'statics/derkash.jpg',
+    avatar: '../assets/derkash.jpg',
     name: 'Vlad'
   }
 ]
